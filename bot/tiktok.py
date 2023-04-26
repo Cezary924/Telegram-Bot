adabf29764d2fef4a08e09e8c65ade8a057b816a
Cezary924 <cezary924@gmail.com>
2023-04-26 20:44:47 +0200

    Add new lines to make code easier to read

--- bot/tiktok.py
+++ bot/tiktok.py
@@ -31,16 +31,20 @@ def echo_tiktok(message, bot):
         "X-RapidAPI-Key": rapidapi,
         "X-RapidAPI-Host": "tiktok-full-info-without-watermark.p.rapidapi.com"
     }
+    
     bot.send_message(message.chat.id, "Przetwarzanie linku z TikToka... ⏳")
+
     response = requests.request("GET", url, headers=headers, params=querystring)
     if response.status_code != 200:
         bot.send_message(message.chat.id, "Niestety, pobranie filmiku z TikToka nie jest teraz możliwe... Spróbuj poźniej 😞")
         return
+    
     vid_url = response.json()['video'][0]
     response = requests.request("GET", vid_url, headers=headers, params=querystring)
     if response.status_code != 200:
         bot.send_message(message.chat.id, "Niestety, pobranie filmiku z TikToka nie jest teraz możliwe... Spróbuj poźniej 😞")
         return
+    
     vid_name = str(message.chat.id) + str(message.message_id) + ".mp4"
     try:
         with open(vid_name, "wb") as f:
