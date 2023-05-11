ecd21ef45480d3378ef71fee02c01a755ea02b62
Cezary924 <cezary924@gmail.com>
2023-05-11 11:37:00 +0200

    Add logging function

--- bot/func.py
+++ bot/func.py
@@ -1,4 +1,7 @@
+import datetime
+
 def read_file(name, path):
+    print_log('')
     try:
         with open(path) as f:
             x = f.readlines()
@@ -6,3 +9,16 @@ def read_file(name, path):
     except OSError:
         print("Open error: Could not open the \'telegram.txt\' file.")
     return x
+
+def print_log(info, bot_name = None):
+    if bot_name != None:
+        print("|================================================================|")
+        print("|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|")
+        print(" " + bot_name + " has been started. ")
+        print("|++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|")
+        print("|================================================================|")
+        return
+    print(str(datetime.datetime.now().strftime(" %Y-%m-%d %H:%M:%S ")))
+    print(" ---------------------------------------------------------------- ")
+    print(" " + info + " ")
+    print("|================================================================|")
