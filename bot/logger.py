d1e026f704f98e075920efe6d7b5361a33a2a023
Cezary924 <cezary924@gmail.com>
2023-06-07 10:57:56 +0200

    Add Logger classes

---
+++ bot/logger.py
@@ -0,0 +1,27 @@
+import sys, datetime
+
+time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+class LoggerStdout(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open("../log/log_out_" + time + ".log", "a")
+    
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+    
+    def flush(self):
+        pass
+
+class LoggerStderr(object):
+    def __init__(self):
+        self.terminal = sys.stderr
+        self.log = open("../log/log_err_" + time + ".log", "a")
+    
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+    
+    def flush(self):
+        pass
