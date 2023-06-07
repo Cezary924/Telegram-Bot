0c02211bdef6acbadc62e14381f7285b07cc79e3
Cezary924 <cezary924@gmail.com>
2023-06-07 10:58:25 +0200

    Use Logger classes for stdout & stderr

--- bot/bot.py
+++ bot/bot.py
@@ -6,11 +6,15 @@ dir = os.path.dirname(os.path.realpath(__file__)) + "/"
 # change current working directory to 'dir'
 os.chdir(dir)
 
-import func
+import func, logger
 
-# write stdout & stdeer to files
-sys.stdout = func.write_file('stdout.txt', '../stdout.txt', 1)
-sys.stderr = func.write_file('stderr.txt', '../stderr.txt', 1)
+# write stdout & stdeer to file only
+# sys.stdout = func.write_file('stdout.txt', '../stdout.txt', 1)
+# sys.stderr = func.write_file('stderr.txt', '../stderr.txt', 1)
+
+# write stdout & stdeer to both console and file
+sys.stdout = logger.LoggerStdout()
+sys.stderr = logger.LoggerStderr()
 
 # open file containing Telegram token and read from it
 if len(sys.argv) == 2 and sys.argv[1] == "beta":
