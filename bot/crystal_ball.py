d8e6186840d5363660beac5add0c2187b39b56ac
Cezary924 <cezary924@gmail.com>
2023-07-04 11:03:25 +0200

    Add more /crystalball msgs variations

--- bot/crystal_ball.py
+++ bot/crystal_ball.py
@@ -4,11 +4,17 @@ import database
 
 # handle /crystalball command
 def command_crystalball(message, bot):
-    number = crystalball()
-    text = database.get_message_text(message, 'command_crystalball_' + str(number))
+    number1, number2 = crystalball()
+    text = database.get_message_text(message, 'command_crystalball_' + str(number1) + "_" + str(number2))
+    if number1 == 1:
+        text = text + " ✅"
+    elif number1 == 2:
+        text = text + " ❔"
+    else:
+        text = text + " ❌"
     mess = bot.send_message(message.chat.id, "🔮 *" + text + "*", parse_mode= 'Markdown')
     database.register_last_message(mess)
 
-# get random int [1, 3]
+# get random int [1, 3] & another random int [1, 5]
 def crystalball():
-    return random.choice([1, 2, 3])
+    return random.choice([1, 2, 3]), random.choice([1, 2, 3, 4, 5])
