e8e06389146f4303f74f9f589c4863ba1ae16f5e
Cezary924 <cezary924@gmail.com>
2023-07-11 12:02:39 +0200

    Add missing comments

--- bot/locales.py
+++ bot/locales.py
@@ -1,5 +1,6 @@
 import yaml
 
+# open and read from locale file
 def load_locale_file(path):
     try:
         with open(path, encoding='utf8') as f:
@@ -9,5 +10,6 @@ def load_locale_file(path):
         print("Open error: Could not open the locale file.")
     return x
 
+# create lang dicts
 pl = load_locale_file("../locales/pl.yaml")
 en = load_locale_file("../locales/en.yaml")
