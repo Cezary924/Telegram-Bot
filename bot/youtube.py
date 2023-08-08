e981533f03840fb018e1ba7057ab48bc332b8041
Cezary924 <cezary924@gmail.com>
2023-08-08 07:37:36 +0200

    Refactoring youtube.py file

--- bot/youtube.py
+++ bot/youtube.py
@@ -1,29 +1,29 @@
 import os
 from pytube import YouTube
-from urllib.parse import urlparse
 
-import database
+import func, downloader
 
 # handle Youtube URLs
+def start_youtube(message, bot):
     url = message.text
+    downloader.send_start_message(bot, message, 'youtube')
 
-    text = database.get_message_text(message, 'youtube_url_start')
-    bot.send_message(message.chat.id, text)
-
+    # downloading vid
+    downloader.send_processing_message(bot, message, 'youtube')
     vid_name = str(message.chat.id) + str(message.message_id) + ".mp4"
-    YouTube(url).streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(filename=vid_name)
-    
-    text = database.get_message_text(message, 'youtube_url_processing')
-    bot.send_message(message.chat.id, text)
+    try:
+        YouTube(url).streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(filename=vid_name)
+    except Exception as err:
+        func.print_log("ERROR: Module error - YouTube.")
+        downloader.send_error_message(bot, message, 'youtube')
+        return
 
+    # sending vid
     try:
         with open(vid_name, 'rb') as f:
             bot.send_video(message.chat.id, f, timeout=10000)
     except Exception as err:
-        text = database.get_message_text(message, 'youtube_url_error')
-        bot.send_message(message.chat.id, text)
+        func.print_log("ERROR: Open error - Could not open the \'.mp4\' file.")
+        downloader.send_error_message(bot, message, 'youtube')
     finally:
         os.remove(vid_name)
-
-def start_youtube(message, bot):
-    echo_youtube(message, bot)
