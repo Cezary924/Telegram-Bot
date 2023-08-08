fd248c8d4237f630f78a4a7250dacb0d04e6f042
Cezary924 <cezary924@gmail.com>
2023-08-08 07:37:52 +0200

    Refactoring tumblr.py file

--- bot/tumblr.py
+++ bot/tumblr.py
@@ -1,47 +1,41 @@
-import requests, os
-from urllib.parse import urlparse
+import os, requests
 
-import database, func
+import func, downloader
 
 # handle Tumblr URLs
+def start_tumblr(message, bot):
     url = message.text
+    downloader.send_start_message(bot, message, 'tumblr')
 
-    text = database.get_message_text(message, 'tumblr_url_start')
-    bot.send_message(message.chat.id, text)
-
+    # downloading vid
     response = requests.request("GET", url)
     if response.status_code != 200:
-        text = database.get_message_text(message, 'tumblr_url_error')
-        bot.send_message(message.chat.id, text)
+        downloader.send_error_message(bot, message, 'tumblr')
         return
-
     if "<meta data-rh=\"\" property=\"og:video\" content=\"" not in response.text:
-        text = database.get_message_text(message, 'tumblr_url_error')
-        bot.send_message(message.chat.id, text)
+        downloader.send_error_message(bot, message, 'tumblr')
         return
-        
     vid_url = response.text.split("<meta data-rh=\"\" property=\"og:video\" content=\"")[1]
     vid_url = vid_url.split("\"/>")[0]
-
     response = requests.request("GET", vid_url)
     if response.status_code != 200:
-        text = database.get_message_text(message, 'tumblr_url_error')
-        bot.send_message(message.chat.id, text)
+        downloader.send_error_message(bot, message, 'tumblr')
         return
-
+    #downloader.send_processing_message(bot, message, 'tumblr')
     vid_name = str(message.chat.id) + str(message.message_id) + ".mp4"
     try:
         with open(vid_name, "wb") as f:
             f.write(response.content)
     except OSError:
         func.print_log("ERROR: Open error - Could not open the \'.mp4\' file.")
+        downloader.send_error_message(bot, message, 'tumblr')
+    
+    # sending vid
     try:
         with open(vid_name, "rb") as f:
             bot.send_video(message.chat.id, f)
     except OSError:
         func.print_log("ERROR: Open error - Could not open the \'.mp4\' file.")
+        downloader.send_error_message(bot, message, 'tumblr')
     finally:
         os.remove(vid_name)
-
-def start_tumblr(message, bot):
-    echo_tumblr(message, bot)
