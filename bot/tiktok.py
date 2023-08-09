693aa84124e46aa39a768abe348e8051b8db8175
Cezary924 <cezary924@gmail.com>
2023-08-09 09:43:44 +0200

    Refactoring tiktok.py file

--- bot/tiktok.py
+++ bot/tiktok.py
@@ -1,59 +1,44 @@
 import requests, os
-from urllib.parse import urlparse
 
-import database, func
-
-rapidapi = None
-
-# open file containing RapidAPI key and read from it
-def read_rapidapi():
-    global rapidapi
-    rapidapi = func.tokens['tiktok']
+import func, downloader
 
 # handle TikTok URLs
-def echo_tiktok(message, bot):
+def start_tiktok(message, bot):
     url = "https://tiktok-full-info-without-watermark.p.rapidapi.com/vid/index"
     querystring = {"url": message.text}
     headers = {
-        "X-RapidAPI-Key": rapidapi,
+        "X-RapidAPI-Key": func.tokens['tiktok'],
         "X-RapidAPI-Host": "tiktok-full-info-without-watermark.p.rapidapi.com"
     }
+    downloader.send_start_message(bot, message, 'tiktok')
 
-    text = database.get_message_text(message, 'tiktok_url_start')
-    bot.send_message(message.chat.id, text)
-
+    # downloading vid
     response = requests.request("GET", url, headers=headers, params=querystring)
     if response.status_code != 200:
-        text = database.get_message_text(message, 'tiktok_url_error')
-        bot.send_message(message.chat.id, text)
+        func.print_log("ERROR: Module error - TikTok.")
+        downloader.send_error_message(bot, message, 'tiktok')
         return
-    
     vid_url = response.json()['video'][0]
     response = requests.request("GET", vid_url, headers=headers, params=querystring)
     if response.status_code != 200:
-        text = database.get_message_text(message, 'tiktok_url_error')
-        bot.send_message(message.chat.id, text)
+        func.print_log("ERROR: Module error - TikTok.")
+        downloader.send_error_message(bot, message, 'tiktok')
         return
-    
     vid_name = str(message.chat.id) + str(message.message_id) + ".mp4"
     try:
         with open(vid_name, "wb") as f:
             f.write(response.content)
     except OSError:
         func.print_log("ERROR: Open error - Could not open the \'.mp4\' file.")
+        downloader.send_error_message(bot, message, 'tiktok')
+        return
+    
+    # sending vid
     try:
         with open(vid_name, "rb") as f:
             bot.send_video(message.chat.id, f)
     except OSError:
         func.print_log("ERROR: Open error - Could not open the \'.mp4\' file.")
+        downloader.send_error_message(bot, message, 'tiktok')
     finally:
         os.remove(vid_name)
-
-def start_tiktok(message, bot):
-    if rapidapi == None:
-        read_rapidapi()
-    if rapidapi != None:
-        echo_tiktok(message, bot)
-    else:
-        text = database.get_message_text(message, 'tiktok_url_error')
-        bot.send_message(message.chat.id, text)
