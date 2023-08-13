57493f5cdbb8d8b8f28fe8adf50e33a3ac33aee0
Cezary924 <cezary924@gmail.com>
2023-08-13 09:16:17 +0200

    Add info about parameter type names - downloader.py

--- bot/downloader.py
+++ bot/downloader.py
@@ -1,23 +1,24 @@
+import telebot
 import database, func
 
 # check URL scheme & URL hostname
-def check_url(message, scheme, hostname):
+def check_url(message: telebot.types.Message, scheme: str, hostname: str):
     return func.check_url(message, scheme, hostname)
 
 # send message at the beginning of the downloading process
-def send_start_message(bot, message, downloader_name):
+def send_start_message(bot: telebot.TeleBot, message: telebot.types.Message, downloader_name: str) -> None:
     text1 = database.get_message_text(message, downloader_name)
     text2 = database.get_message_text(message, 'downloader_start')
     bot.send_message(message.chat.id, text1 + text2, parse_mode= 'Markdown')
 
 # send message in the middle of the downloading process
-def send_processing_message(bot, message, downloader_name):
+def send_processing_message(bot: telebot.TeleBot, message: telebot.types.Message, downloader_name: str) -> None:
     text1 = database.get_message_text(message, downloader_name)
     text2 = database.get_message_text(message, 'downloader_processing')
     bot.send_message(message.chat.id, text1 + text2, parse_mode= 'Markdown')
 
 # send message about error during the downloading process
-def send_error_message(bot, message, downloader_name):
+def send_error_message(bot: telebot.TeleBot, message: telebot.types.Message, downloader_name: str) -> None:
     text1 = database.get_message_text(message, downloader_name)
     text2 = database.get_message_text(message, 'downloader_error')
     bot.send_message(message.chat.id, text1 + text2, parse_mode= 'Markdown')
