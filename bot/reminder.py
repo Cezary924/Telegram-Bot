6699d7354ef9dbad2a4f9e8116ff43d2c9fb4e6a
Cezary924 <cezary924@gmail.com>
2023-08-21 16:58:21 +0200

    Add /reminder command handler

--- bot/reminder.py
+++ bot/reminder.py
@@ -1,8 +1,10 @@
-import time
+import telebot, time
 from threading import Thread
 from datetime import datetime
 
-def check_reminders():
+import database
+
+def check_reminders() -> None:
     while True:
         t = datetime.utcnow()
         sleeptime = 60 - (t.second + t.microsecond/1000000.0)
@@ -11,3 +13,41 @@ def check_reminders():
 
 thread = Thread(target = check_reminders, daemon = True)
 thread.start()
+
+# handle /reminder command
+def command_reminder(message: telebot.types.Message, bot: telebot.TeleBot) -> None:
+    number, reminders = database.get_reminders(message)
+    text1 = database.get_message_text(message, 'reminder')
+    text2 = database.get_message_text(message, 'command_reminder')
+    markup = telebot.types.InlineKeyboardMarkup()
+    text3 = database.get_message_text(message, 'reminder_set_button')
+    create_button = telebot.types.InlineKeyboardButton(text = text3, callback_data = "command_reminder_set")
+    markup.add(create_button)
+    if number > 0:
+        text3 = database.get_message_text(message, 'reminder_manage_button')
+        manage_button = telebot.types.InlineKeyboardButton(text = text3, callback_data = "command_reminder_manage")
+        markup.add(manage_button)
+    text3 = database.get_message_text(message, 'exit')
+    exit_button = telebot.types.InlineKeyboardButton(text = text3, callback_data = "command_reminder_return")
+    markup.add(exit_button)
+    mess = bot.send_message(message.chat.id, "🔔 *" + text1 + ":*\n\n" + text2 + ": _" + str(number) + "_", reply_markup = markup, parse_mode= 'Markdown')
+    database.register_last_message(mess)
+
+def command_reminder_set(message: telebot.types.Message, bot: telebot.TeleBot) -> None:
+    text1 = database.get_message_text(message, 'reminder')
+    text2 = database.get_message_text(message, 'reminder_set_button')
+    text3 = database.get_message_text(message, 'command_reminder_set')
+    mess = bot.send_message(message.chat.id, "🔔 *" + text1 + " > " + text2 + ":*\n\n" + text3 + ":", parse_mode='Markdown')
+    database.register_last_message(mess)
+
+def command_reminder_manage(message: telebot.types.Message, bot: telebot.TeleBot) -> None:
+    text1 = database.get_message_text(message, 'reminder')
+    text2 = database.get_message_text(message, 'reminder_manage_button')
+    text3 = database.get_message_text(message, 'command_reminder_manage')
+    mess = bot.send_message(message.chat.id, "🔔 *" + text1 + " > " + text2 + ":*\n\n" + text3 + ":", parse_mode='Markdown')
+    database.register_last_message(mess)
+
+def command_reminder_return(message: telebot.types.Message, bot: telebot.TeleBot) -> None:
+    text = database.get_message_text(message, 'command_reminder_return')
+    mess = bot.send_message(message.chat.id, text, parse_mode='Markdown')
+    database.register_last_message(mess)
